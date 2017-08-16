1be780f43043902d4b04b8ae6dc45d0e31cf971d
MauricioAcosta <andres.taamap@gmail.com>
2017-08-16 07:42:56 -0500

    ordenado archivos y creación de arbol binario en preorden

---
+++ arbol_binario/arbolbinario.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+class Nodo():
+	def __init__ (self,valor,izq=None,der=None):
+		self.valor=valor
+		self.izquierda=izq
+		self.derecha=der
+
+def preorden(arbol):
+	if arbol==None:
+		return ""
+	else:
+		return arbol.valor+preorden(arbol.izquierda)+preorden(arbol.derecha)
+
+def buscar(arbol,valor):
+	if arbol==None:
+		return False
+	elif arbol.valor==valor:
+		return True
+	else:
+		return buscar(arbol.izquierda) or buscar(arbol.derecha)
+
+def evaluar(arbol):
+	if arbol.valor=='+':
+		return evaluar(arbol.izquierda)+evaluar(arbol.derecha)
+	elif arbol.valor=='-':
+		return evaluar(arbol.izquierda)-evaluar(arbol.derecha)
+	elif arbol.valor=='*':
+		return evaluar(arbol.izquierda)*evaluar(arbol.derecha)
+	elif arbol.valor=='/':
+		return evaluar(arbol.izquierda)/evaluar(arbol.derecha)
+	else:
+		return int(arbol.valor)
+
+print (preorden(Nodo('+',Nodo('5'),Nodo('-',Nodo('8'),Nodo('6')))))
+print (evaluar(Nodo('+',Nodo('5'),Nodo('-',Nodo('8'),Nodo('6')))))
