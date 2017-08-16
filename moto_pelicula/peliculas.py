587e624020fe624d46cb019944c97f25b3c35ef8
MauricioAcosta <andres.taamap@gmail.com>
2017-08-16 07:03:01 -0500

    paquetes

---
+++ moto_pelicula/peliculas.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append("..")
+from Pila import *
+
+class Peliculas:
+
+    def __init__(self,year,gender,author):
+        self.year=year
+        self.gender=gender
+        self.author=author
+
+pila = Pila()
+
+pila.apilar(Peliculas(2001,"fantasy","Andres"))
+pila.apilar(Peliculas(2012,"romance","Hasta"))
+pila.apilar(Peliculas(2010,"accion","Nicolas"))
+pila.apilar(Peliculas(2001,"horror","Arnold"))
+
+while pila.es_vacia() == False:
+    print pila.desapilar().author
+print "termine"
+print "La pila esta vacia:",pila.es_vacia()
